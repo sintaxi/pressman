b895a9385d28b6a9edc66d46402e61ef8d43e3f7
Kenneth Ormandy <kenneth@chloi.io>
2015-05-27 18:32:51 -0700

    Adds media query to tests

--- test/basic.js
+++ test/basic.js
@@ -46,6 +46,7 @@ describe("email", function(){
       body.html.should.not.include("<link")
       body.html.should.not.include("<style type=\"text/css\">h1 {")
       body.html.should.include("<h1 style=\"color: blue;\">")
+      body.html.should.include("<style type=\"text/css\">@media")
       done()
     })
   })
